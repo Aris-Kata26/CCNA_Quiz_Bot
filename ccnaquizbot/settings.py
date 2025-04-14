a2d0937611afad073d1bddf5f935a2e328f53e8c
Aris-Kata26 <katar711@school.lu>
2025-04-14 11:09:53 +0200

    updates

--- ccnaquizbot/settings.py
+++ ccnaquizbot/settings.py
@@ -135,9 +135,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET')
+    'CLOUD_NAME': os.getenv('Aristide'),
+    'API_KEY': os.getenv('592164123429527'),
+    'API_SECRET': os.getenv('wgABnabD7pyoZXlbTyRWH1gJaFE')
 }
 
 # Media files (Uploaded images)
